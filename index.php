47f13f181872d7d618f77b9e9cd7ec40b724f971
Lokendra <lokendraadi1@gmail.com>
2020-03-29 20:19:41 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur rem beatae quis consectetur recusandae ad nisi ratione quam! Fugit suscipit blanditiis exercitationem voluptate ipsa accusantium ipsum in quibusdam aliquam cupiditate.</p>
 </body>
 </html>
