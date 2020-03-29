9d079f0442e3c4cdb9393b746ca78109265e4373
Lokendra <lokendraadi1@gmail.com>
2020-03-29 20:29:11 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur rem beatae quis consectetur recusandae ad nisi ratione quam! Fugit suscipit blanditiis exercitationem voluptate ipsa accusantium ipsum in quibusdam aliquam cupiditate.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
